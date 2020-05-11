ce717a56026555a87a188f52b7b1aef96db685ba
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-05-11 10:15:37 -0300

    arrays

---
+++ aula15/vetornatela.js
@@ -0,0 +1,18 @@
+let valores = [8, 1, 7, 4, 2, 9]
+valores.sort()
+    // console.log(valores)
+
+// console.log(valores[0])
+// console.log(valores[1])
+// console.log(valores[2])
+// console.log(valores[3])
+// console.log(valores[4])
+// console.log(valores[5])
+
+// for (let i = 0; i < valores.length; i++) {
+//     console.log(`A posição ${i} tem o valor ${valores[i]}`)
+// }
+
+for (let i in valores) {
+    console.log(`A posição ${i} tem o valor ${valores[i]}`)
+}
